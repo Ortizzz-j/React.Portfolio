aafa02a53bd1dcd16c336ca74962c129a814684d
Ortizzz-j <124862900+Ortizzz-j@users.noreply.github.com>
2023-08-12 09:06:15 -0700

    lowercased the a in about import

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import './About.scss'
+import './about.scss'
 const About = () => {
     return (
         <div>
