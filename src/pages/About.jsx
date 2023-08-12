4e4e7b3091f4f43d262538e02ab3d4057e31ff9e
Ortizzz-j <124862900+Ortizzz-j@users.noreply.github.com>
2023-08-12 09:24:51 -0700

    pics part V

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import './about.scss'
-import Mau from '../components/assets/Mau.jpeg'
+import Mau from '../components/assets/Mau.png'
 
 const About = () => {
     return (
