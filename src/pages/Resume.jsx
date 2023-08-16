54441d223357671b9547041b53116dd103154fcd
Ortizzz-j <124862900+Ortizzz-j@users.noreply.github.com>
2023-08-16 15:32:01 -0700

    took out the button element

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -13,27 +13,26 @@ const Resume = () => {
             <Container fluid>
                 <Row>
                     <Col>
-                    <h1 className="d-flex justify-content-center">Resume</h1>
+                        <h1 className="d-flex justify-content-center">Resume</h1>
                     </Col>
                 </Row>
                 <Row>
                     <Col>
-                    <h2>Technologies</h2>
-                    <ul>{tech.map((tech, index)=>(
-                        <li key={index}>{tech}</li>
-                    ))}</ul>
+                        <h2>Technologies</h2>
+                        <ul>{tech.map((tech, index) => (
+                            <li key={index}>{tech}</li>
+                        ))}</ul>
                     </Col>
                     <Col>
-                    <h2>Road Map post Bootcamp</h2>
-                    <ul>
-                        {rdmp.map((rdmp, index) => (
-                            <li key={index}>{rdmp}</li>
-                        ))}
-                    </ul>
-                    <button className="btn btn-success">
-                        Download Resume!
-                        <a href={resume} download="MauriciosResume"></a>
-                    </button>
+                        <h2>Road Map post Bootcamp</h2>
+                        <ul>
+                            {rdmp.map((rdmp, index) => (
+                                <li key={index}>{rdmp}</li>
+                            ))}
+                        </ul>
+                        <a href={resume} download="MauriciosResume" className="btn btn-success">
+                            Download Resume!
+                        </a>
                     </Col>
                 </Row>
             </Container>
